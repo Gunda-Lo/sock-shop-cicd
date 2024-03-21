eb532c1340492fd6e7e0a045dc6aa8a6fcbcd367
Gunda-Lo <segeboi@gmail.com>
2024-03-21 16:22:17 +0100

    Modified backeend.tf and provider.tf

--- code-to-create-jenkins-server/provider.tf
+++ code-to-create-jenkins-server/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = data.vault_generic_secret.aws_creds.data["AWS_REGION"]
+  region = var.region
   access_key = data.vault_generic_secret.aws_creds.data["AWS_ACCESS_KEY_ID"]
   secret_key = data.vault_generic_secret.aws_creds.data["AWS_SECRET_ACCESS_KEY"]
 }
