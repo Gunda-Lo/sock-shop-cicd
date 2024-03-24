bbffa79e6913cd3576ada3fa30cda1c142603d6a
Gunda-Lo <segeboi@gmail.com>
2024-03-24 05:29:26 +0100

    added iam.tf edited variables.tf and eks-cluster.tf

--- socks-shop-eks-deployment/terraform/eks-cluster.tf
+++ socks-shop-eks-deployment/terraform/eks-cluster.tf
@@ -7,30 +7,33 @@ module "eks" {
 
   cluster_endpoint_public_access  = true
 
-
   vpc_id                   = "vpc-06a057e557b4a6f9f"
   subnet_ids               = module.jenkins-server-vpc.private_subnets
- 
 
   eks_managed_node_groups = {
     one= {
-        name = "master-node-group"
+      name = "master-node-group"
 
       instance_types = ["t2.medium"]
           
       min_size     = 1
       max_size     = 4
       desired_size = 2
-  }
+    }
 
-  two= {
-        name = "worker-node-group"
+    two= {
+      name = "worker-node-group"
 
       instance_types = ["t2.medium"]
           
       min_size     = 1
       max_size     = 4
       desired_size = 2
+    }
   }
 }
+
+resource "aws_cloudwatch_log_group" "eks_cluster_logs" {
+  name              = "/aws/eks/${var.cluster_name}/cluster"
+  retention_in_days = 7  // Adjust retention period as needed
 }
--- socks-shop-eks-deployment/terraform/variables.tf
+++ socks-shop-eks-deployment/terraform/variables.tf
@@ -1,9 +1,27 @@
-variable "private_subnets_cidr_blocks" {}
-variable "public_subnets_cidr_blocks" {}
-variable "vpc_cidr_block" {}
-variable "cluster_name" {}
+
+Copy code
+variable "private_subnets_cidr_blocks" {
+  type    = list(string)
+  default = ["100.0.1.0/24", "100.0.2.0/24", "100.0.3.0/24"]
+}
+
+variable "public_subnets_cidr_blocks" {
+  type    = list(string)
+  default = ["100.0.101.0/24", "100.0.102.0/24", "100.0.103.0/24"]
+}
+
+variable "vpc_cidr_block" {
+  type    = string
+  default = "100.0.0.0/16"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "sock-shop-eks-cluster"
+}
+
 variable "region" {
-    description = "EKS cluster region"
-    type = string
-    default = "us-east-1"
-  }
+  description = "EKS cluster region"
+  type        = string
+  default     = "us-east-1"
+}
