f448bb3e25039995c7c902f3442dddb28780b2e2
Gunda-Lo <segeboi@gmail.com>
2024-03-24 21:33:24 +0100

    edited vpctf

--- socks-shop-eks-deployment/terraform/vpc.tf
+++ socks-shop-eks-deployment/terraform/vpc.tf
@@ -28,3 +28,32 @@ module "jenkins-server-vpc" {
   
 
 }
+resource "aws_security_group" "jenkins-server-sg" {
+  vpc_id      = module.jenkins-server-vpc.default_vpc_id
+  description = "Allowing Jenkins, Sonarqube, SSH Access"
+
+  ingress = [
+    for port in [22, 8080, 9000, 9090, 80] : {
+      description      = "TLS from VPC"
+      from_port        = port
+      to_port          = port
+      protocol         = "tcp"
+      ipv6_cidr_blocks = ["::/0"]
+      self             = false
+      prefix_list_ids  = []
+      security_groups  = []
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+  ]
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = ""
+  }
+}
