374a4f0a2bd1e038da33ca4f726dd46e07a99c06
Gunda-Lo <segeboi@gmail.com>
2024-03-28 05:31:11 +0100

    added outputs for subnets id

--- socks-shop-eks-deployment/terraform/outputs.tf
+++ socks-shop-eks-deployment/terraform/outputs.tf
@@ -17,3 +17,13 @@ output "cluster_security_group_id" {
     description = "EKS cluster security group id"
     value = module.eks.cluster_security_group_id
 }
+
+output "private_subnets_id" {
+  description = "Eks cluster private subnets id"
+  value = module.eks-vpc.private_subnets_id
+ }
+
+ output "private_subnets_id" {
+  description = "Eks cluster prpublicivate subnets id"
+  value = module.eks-vpc.public_subnets_id
+ }
